60abd31ec1f3c2713121d7c030b3b409b9dd413c
Kyeongmin Kim <kkfenix3085@naver.com>
2021-05-13 23:46:53 +0900

    Thread Practice

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ServerCore
 {
     class Program
     {
+        static void MainThread()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hello, Thread!");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Server Core");
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(10, 10);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Task t = new Task(() => { Console.WriteLine("A"); });
+                t.Start();
+            }
+
+
+
+            // for (int i = 0; i < 10; i++)
+            // {
+            //     // Console.WriteLine($"{i}");
+            //     ThreadPool.QueueUserWorkItem(MainThread);
+            // }
+
+
+            // for (int i = 0; i < 1000; i++)
+            // {
+            //     Thread t = new Thread(MainThread);
+            //     t.Name = $"Test Thread{i}";
+            //     t.IsBackground = true;
+
+            //     Console.WriteLine($"Thread{i}: start");
+            //     t.Start();
+            //     Console.WriteLine($"Thread{i}: Done");
+            // }
         }
     }
 }
