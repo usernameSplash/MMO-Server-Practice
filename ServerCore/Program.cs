531d84352940f2e4b913b137c2b3385e7d984f5e
Kyeongmin Kim <kkfenix3085@naver.com>
2021-05-16 03:50:00 +0900

    ReaderWriterLock 테스트

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,56 +4,37 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    class Lock
-    {
-        AutoResetEvent _available = new AutoResetEvent(true);
-
-        public void Acquire()
-        {
-            _available.WaitOne();
-        }
-
-        public void Release()
-        {
-            _available.Set();
-        }
-    }
     class Program
     {
-        static int _num = 0;
+        static volatile int count = 0;
         static Lock _lock = new Lock();
-
-        static void Thread1()
-        {
-            for (int i = 0; i < 1000000; i++)
-            {
-                _lock.Acquire();
-                _num++;
-                _lock.Release();
-            }
-        }
-
-        static void Thread2()
-        {
-            for (int i = 0; i < 1000000; i++)
-            {
-                _lock.Acquire();
-                _num--;
-                _lock.Release();
-            }
-        }
-
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread1);
-            Task t2 = new Task(Thread2);
+            Task t1 = new Task(delegate ()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count++;
+                    _lock.WriteUnlock();
+                }
+            });
+
+            Task t2 = new Task(delegate ()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count--;
+                    _lock.WriteUnlock();
+                }
+            });
 
             t1.Start();
             t2.Start();
-
             Task.WaitAll(t1, t2);
 
-            Console.WriteLine(_num);
+            Console.WriteLine(count);
         }
     }
 }
