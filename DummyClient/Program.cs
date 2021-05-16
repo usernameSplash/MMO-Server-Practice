685fda2d259459153afa20d1daf96548e8f3e0f6
Kyeongmin Kim <kkfenix3085@naver.com>
2021-05-17 01:05:43 +0900

    Socket Programming Tutorial Basic Server and Client Test

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
 
 namespace DummyClient
 {
@@ -6,7 +9,38 @@ namespace DummyClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("DummyClient");
+            // DNS (Domain Name System)
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            // Socket Setting
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                socket.Connect(endPoint);
+
+                Console.WriteLine($"Connected to {socket.RemoteEndPoint.ToString()}");
+
+                byte[] sendBuffer = Encoding.UTF8.GetBytes("Hello, World!");
+                int sendBytes = socket.Send(sendBuffer);
+
+                byte[] receiveBuffer = new byte[1024];
+                int receiveBytes = socket.Receive(receiveBuffer);
+                string receiveData = Encoding.UTF8.GetString(receiveBuffer, 0, receiveBytes);
+
+                Console.WriteLine($"[From Server]: {receiveData}");
+
+                socket.Shutdown(SocketShutdown.Both);
+
+                socket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,36 +1,60 @@
 ﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ServerCore
 {
     class Program
     {
-        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(
-            () =>
-            {
-                return $"My Name is {Thread.CurrentThread.ManagedThreadId}";
-            }
-        );
-
-        static void WhoAmI()
-        {
-            // 스레드 개인 영역에서만 ThreadName이 변경된 상태이다
-            Thread.Sleep(10);
-
-            Console.WriteLine(ThreadName.Value);
-        }
-
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(15, 15);
+            // DNS (Domain Name System)
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            Parallel.Invoke(
-                WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI,
-                WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI,
-                WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI
-            );
+            Console.WriteLine(host);
+
+            // Socket Setting
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // Socket에 ip 주소 연동
+                listenSocket.Bind(endPoint);
+
+                // 통신 시작
+                // backlog : 최대 대기 수
+                listenSocket.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Listening...");
+
+                    // Client 접속
+                    Socket clientSocket = listenSocket.Accept();
+
+                    // Client 요청 받기
+                    byte[] receiveBuffer = new byte[1024];
+                    int receiveBytes = clientSocket.Receive(receiveBuffer);
+                    string receiveData = Encoding.UTF8.GetString(receiveBuffer, 0, receiveBytes);
+                    Console.WriteLine($"[From Client]: {receiveData} {DateTime.Now}");
+
+                    // Client에게 보내기
+                    byte[] sendBuffer = Encoding.UTF8.GetBytes("Welcome to Kyeongmin's server!");
+                    clientSocket.Send(sendBuffer);
+
+                    // Client 퇴장
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
