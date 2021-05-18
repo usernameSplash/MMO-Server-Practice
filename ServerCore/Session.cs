14bae852227698fdd998fb7841ef629ccef897bf
Kyeongmin Kim <kkfenix3085@naver.com>
2021-05-19 02:08:06 +0900

    Asynchronized Send

--- ServerCore/Session.cs
+++ ServerCore/Session.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Collections.Generic;
 
 namespace ServerCore
 {
@@ -10,6 +11,11 @@ namespace ServerCore
     {
         Socket _socket;
         int _disconnected = 0;
+
+        object _lock = new Object();
+        Queue<byte[]> _sendQueue = new Queue<byte[]>();
+        bool _pending = false;
+        SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
 
         public void Init(Socket socket)
         {
@@ -19,17 +25,62 @@ namespace ServerCore
             recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnReceiveCompleted);
             recvArgs.SetBuffer(new byte[1024], 0, 1024);
 
+            _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
+
             RegisterRecieve(recvArgs);
+
         }
 
         public void Send(byte[] sendBuff)
         {
-            _socket.Send(sendBuff);
+            lock (_lock)
+            {
+                _sendQueue.Enqueue(sendBuff);
+                if (_pending == false)
+                    RegisterSend();
+            }
         }
         public void Send(string msg)
         {
             byte[] sendBuff = Encoding.UTF8.GetBytes(msg);
             Send(sendBuff);
+        }
+
+        void RegisterSend()
+        {
+            _pending = true;
+
+            byte[] buff = _sendQueue.Dequeue();
+            _sendArgs.SetBuffer(buff, 0, buff.Length);
+
+            bool pending = _socket.SendAsync(_sendArgs);
+            if (pending == false)
+                OnSendCompleted(null, _sendArgs);
+        }
+
+        void OnSendCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            lock (_lock)
+            {
+                if (args.BytesTransferred > 0 && (args.SocketError == SocketError.Success))
+                {
+                    try
+                    {
+                        if (_sendQueue.Count > 0)
+                            RegisterSend();
+                        else
+                            _pending = false;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"OnSendCompleted Failed {e}");
+                    }
+                }
+                else
+                {
+                    // Disconnect
+                }
+            }
         }
 
         public void Disconnect()
@@ -69,7 +120,7 @@ namespace ServerCore
             }
             else
             {
-                // Todo: Disconnected
+                Disconnect();
             }
         }
         #endregion
