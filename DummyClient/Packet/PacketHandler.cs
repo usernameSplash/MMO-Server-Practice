b52bcfe8c198800b4e41803c6591b7cc5f530ceb
Kyeongmin Kim <kkfenix3085@naver.com>
2021-06-09 01:26:25 +0900

    Define Packet for Chatting

--- DummyClient/Packet/PacketHandler.cs
+++ DummyClient/Packet/PacketHandler.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using DummyClient;
 using ServerCore;
 
 class PacketHandler
 {
     public static void S_ChatHandler(Session session, IPacket packet)
     {
+        S_Chat chatPacket = packet as S_Chat;
+        ServerSession s = session as ServerSession;
 
+        // if (chatPacket.playerID == 1)
+        Console.WriteLine(chatPacket.chat);
     }
 }
--- Server/Packet/PacketHandler.cs
+++ Server/Packet/PacketHandler.cs
@@ -1,13 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Server;
 using ServerCore;
 
 class PacketHandler
 {
     public static void C_ChatHandler(PacketSession session, IPacket packet)
     {
+        C_Chat chatPacket = packet as C_Chat;
+        ClientSession clientSession = session as ClientSession;
 
+        if (clientSession.Room == null)
+            return;
+
+        clientSession.Room.Broadcast(clientSession, chatPacket.chat);
     }
 
 }
