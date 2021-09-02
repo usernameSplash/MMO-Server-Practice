ce97ec0d45b053a9081d5fa2de4fe03b677698ae
Kyeongmin Kim <kkfenix3085@naver.com>
2021-09-02 23:37:04 +0900

    Woriking with Unity Engine

---
+++ Client/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,36 @@
+﻿using DummyClient;
+using ServerCore;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    ServerSession _session = new ServerSession();
+
+    void Start()
+    {
+        Console.WriteLine("ABCD");
+        // DNS (Domain Name System)
+        string host = "localhost";
+
+        IPHostEntry ipHost = Dns.GetHostEntry(host);
+        IPAddress ipAddr = ipHost.AddressList[0];
+        IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+        Console.WriteLine(ipAddr);
+
+        Connector connector = new Connector();
+
+        connector.Connect(endPoint,
+            () => { return _session; },
+            1);
+    }
+
+    void Update()
+    {
+
+    }
+}
