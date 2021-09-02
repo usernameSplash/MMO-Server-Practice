f98428c391df67f2bf5794d6710206723c0df451
Kyeongmin Kim <kkfenix3085@naver.com>
2021-09-03 00:07:06 +0900

    Working with Unity Engine

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -7,37 +7,37 @@ using System.Threading;
 
 namespace DummyClient
 {
+	
 
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// DNS (Domain Name System)
+			string host = Dns.GetHostName();
+			IPHostEntry ipHost = Dns.GetHostEntry(host);
+			IPAddress ipAddr = ipHost.AddressList[0];
+			IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // DNS (Domain Name System)
-            string host = Dns.GetHostName();
-            IPHostEntry ipHost = Dns.GetHostEntry(host);
-            IPAddress ipAddr = ipHost.AddressList[0];
-            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+			Connector connector = new Connector();
 
-            Connector connector = new Connector();
+			connector.Connect(endPoint, 
+				() => { return SessionManager.Instance.Generate(); },
+				10);
 
-            connector.Connect(endPoint,
-                () => { return SessionManager.Instance.Generate(); },
-                10);
+			while (true)
+			{
+				try
+				{
+					SessionManager.Instance.SendForEach();
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.ToString());
+				}
 
-            while (true)
-            {
-                try
-                {
-                    SessionManager.Instance.SendForEach();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.ToString());
-                }
-
-                Thread.Sleep(250);
-            }
-        }
-    }
+				Thread.Sleep(250);
+			}
+		}
+	}
 }
